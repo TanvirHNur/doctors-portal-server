31f549caa841ec2178f8eda48dd29a97968a606e
TanvirHNur <tanvir.du.ier@gmail.com>
2021-11-11 12:55:38 +0600

    added AdminRoute

--- index.js
+++ index.js
@@ -39,6 +39,18 @@ async function run(){
           // console.log(result)
           res.send(result)
         });
+
+        app.get('/users/:email', async (req,res) => {
+          const email = req.params.email;
+          const query = {email: email};
+          const user = await usersCollection.findOne(query);
+          let isAdmin = false;
+          if (user?.role === 'admin'){
+            isAdmin =true;
+          }
+          res.json({admin: isAdmin})
+        })
+
         app.post('/users', async (req,res) => {
           const user = req.body;
           const result = await usersCollection.insertOne(user);
